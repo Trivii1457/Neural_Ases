204bb6d2240bf9efef0af8c68236a8926bb7c59a
Trivi <trivi1457@gmail.com>
2025-07-16 20:26:10 -0500

    Ligera mejora

--- src/database/db.py
+++ src/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"  # Cambia esto según tu configuración de base de datos
+DATABASE_URL = "sqlite:///./database.db"    
 
 Base = declarative_base()
 
